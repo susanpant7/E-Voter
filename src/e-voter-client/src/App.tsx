c178414598ee5aca41c18307ee88fcb1b851e28d
susanpant7 <susan.pant7@gmail.com>
2025-12-05 15:17:52 -0500

    Moified next js to react js Added tailwind css

---
+++ src/e-voter-client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+      <>
+          <h1 className="text-3xl font-bold underline">
+              E-Voter Client!
+          </h1>
+      </>
+  )
+}
+
+export default App
